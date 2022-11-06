39f12f344df4e843455f67552d1be3a7002122bd
Jared Peterson <53375196+Illusivebyte@users.noreply.github.com>
2022-11-06 08:19:00 -0500

    feat(scripts): add ability to programatically register for event object

--- src/Plugin/EventTools/EventObject.cs
+++ src/Plugin/EventTools/EventObject.cs
@@ -7,30 +7,57 @@ public class EventObject : ScriptableObject
     public bool debug;
 	public delegate void EmptyEventCallBack();
 	public List<EventListener> registeredListeners = new List<EventListener>();
+    public delegate void EventCallbackDelegate();
+    public List<EventCallbackDelegate> registeredDelegates = new List<EventCallbackDelegate>();
 
-	public void RegisterForEvent(EventListener listener){
+	public void RegisterForEvent(EventListener listener)
+    {
 		registeredListeners.Add (listener);
 	}
 
-	public void UnregisterForEvent(EventListener listener){
-		registeredListeners.Remove (listener);
+    public void RegisterForEvent(EventCallbackDelegate del)
+    {
+        registeredDelegates.Add(del);
+    }
+
+	public void UnregisterForEvent(EventListener listener)
+    {
+		registeredListeners.Remove(listener);
 	}
+
+    public void UnRegisterForEvent(EventCallbackDelegate del)
+    {
+        registeredDelegates.Remove(del);
+    }
 
 	public void Activate()
     {
         if(this.debug)
 		    Debug.Log ("Activating Event: " + this.name);
-        for (int i = this.registeredListeners.Count-1; i >= 0 ; i--)
+        for (int i = registeredListeners.Count-1; i >= 0 ; i--)
         {
-            if (this.registeredListeners[i] != null)
+            if (registeredListeners[i] != null)
             {
-                if (this.debug)
-                    Debug.Log("Event " + this.name + "calling: " + this.registeredListeners[i].gameObject.name);
+                if (debug)
+                    Debug.Log("Event " + this.name + "calling: " + registeredListeners[i].gameObject.name);
                 registeredListeners[i].EventActivated();
             }
             else
             {
                 registeredListeners.RemoveAt(i);
+            }
+		}
+        for (int i = registeredDelegates.Count-1; i >= 0 ; i--)
+        {
+            if (registeredDelegates[i] != null)
+            {
+                if (debug)
+                    Debug.Log("Event " + this.name + "calling: " + registeredDelegates[i].Target);
+                registeredDelegates[i].Invoke();
+            }
+            else
+            {
+                registeredDelegates.RemoveAt(i);
             }
 		}
 	}
