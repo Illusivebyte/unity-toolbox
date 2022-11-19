23c41f1e1fdfd5ede8655636f4f810a7a68a7549
Jared Peterson <53375196+Illusivebyte@users.noreply.github.com>
2022-11-19 15:25:49 -0500

    feat(scripts): add panelcontroller helper class

---
+++ src/Tools/PanelController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+[RequireComponent(typeof(CanvasGroup))]
+public class PanelController : MonoBehaviour {
+	
+	protected Animation animationComponent;
+    protected bool active {get; set;}
+    public bool isActive {get {return this.active;}}
+
+    protected virtual void Awake()
+    {
+        animationComponent = this.gameObject.GetComponent<Animation>();
+    }
+
+	public virtual void FadeIn()
+	{
+        this.active = true;
+		this.animationComponent.Play("canvasGroupFadeIn");
+	}
+
+    public virtual void FadeOut()
+	{
+        this.active = false;
+		this.animationComponent.Play("canvasGroupFadeOut");
+	}
+}
